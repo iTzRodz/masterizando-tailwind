37b6cc380cd1914003e58880f5f5e2d52fbf87f5
Rodolfo wsl <rndcsjj@gmail.com>
2024-08-06 08:34:50 -0300

    feat: add to page TextArea component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { SettingsTabs } from '@/components/settingsTabs'
 import * as Input from '@/components/input'
-import { Mail } from 'lucide-react'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import * as FileInput from '@/components/form/fileInput'
 import { Select } from '@/components/form/select'
 import { SelectItem } from '@/components/form/select/item'
+import { TextArea } from '@/components/form/textArea'
 export default function Home() {
   return (
     <>
@@ -137,7 +138,58 @@ export default function Home() {
               Bio
               <span>Write a short introduction.</span>
             </label>
-            <div></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-2 gap-3">
+                <Select placeholder="" defaultValue="normal">
+                  <SelectItem
+                    value="normal"
+                    defaultChecked
+                    text="Normal Text"
+                  />
+                  <SelectItem value="md" text="Markdown" />
+                </Select>
+                <div className="flex items-center gap-1">
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Bold className="size-4 text-zinc-500 " strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Italic className="size-4 text-zinc-500 " strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Link className="size-4 text-zinc-500 " strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <List className="size-4 text-zinc-500 " strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <ListOrdered
+                      className="size-4 text-zinc-500 "
+                      strokeWidth={3}
+                    />
+                  </button>
+                </div>
+              </div>
+
+              <TextArea
+                id="bio"
+                defaultValue={`I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development.`}
+              />
+            </div>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
