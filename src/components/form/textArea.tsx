32793bdaf03c777fb6561608cc6e9c1cb46725d8
Rodolfo wsl <rndcsjj@gmail.com>
2024-08-06 08:34:19 -0300

    feat: create textArea component

---
+++ src/components/form/textArea.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from 'react'
+
+export interface TextAreaProps extends ComponentProps<'textarea'> { }
+export function TextArea(props: TextAreaProps) {
+  return (
+    <textarea
+      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
